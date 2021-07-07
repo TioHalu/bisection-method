e410d813771f6051f045a9b400ee80349e686b46
TioHalu <66395712+TioHalu@users.noreply.github.com>
2021-07-07 07:01:18 +0700

    upload
    sc bisa digunakan di dua code editor yaitu mathlab dan octave

---
+++ MetodeBagiDua.m
@@ -0,0 +1,35 @@
+clear all;clc;close all;
+%Nilai parameter
+xl=0;
+xu=6;
+ErrorToleransi=0.0001;
+ErrorRelatif=1;
+xm0=1;
+it=0;
+
+
+
+while ErrorRelatif>ErrorToleransi
+  it=it+1;
+  xm=(xl+xu)/2;
+  Fbawah=fungsi(xl);
+  Ftengah=fungsi(xm);
+  
+    if Fbawah*Ftengah==0
+      disp('xm adalah akar lansung');
+    elseif Fbawah*Ftengah<0
+      xu=xm;
+    else
+      xl=xm;
+    end
+   ErrorRelatif=abs((xm0-xm)/xm0);
+   xm0=xm;
+   
+  
+end
+fprintf('MetodeBagiDua\n');
+fprintf('iterasi =%3d\n',it);
+fprintf('xm=%8.5f\n',xm);
+fprintf('error relative=%8.5f\n',ErrorRelatif);
+fprintf('Xl=%8.5f\n',xl);
+fprintf('Xu=%8.5f',xu);
---
+++ fungsi.m
@@ -0,0 +1,3 @@
+function y=fungsi(x)
+  y=x^3+x^2-8*x-10;
+end
